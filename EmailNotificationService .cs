f8727978e8b7b199619f51ad309c0e7236b20eb8
BrunoBafilli <brunobafilli@gmail.com>
2024-04-19 19:37:47 -0300

    CleanArch_DIP_DI_IOC
    CleanArch_DIP_DI_IOC

---
+++ EmailNotificationService .cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EmailNotificationService : INotificationService
+    {
+        private string _token;
+        private ValidateEmail _validateEmail;
+
+        public EmailNotificationService(string token, string email)
+        {
+            _validateEmail = new ValidateEmail(email);
+            _token = token;
+        }
+
+        public bool Send(string msg, string email)
+        {
+            if (_token == "1234" && _validateEmail.Validate())
+            {
+                Console.WriteLine($"{msg} para: {email}");
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class SMSNotificationService : INotificationService
+    {
+        private string _token;
+        private ValidateEmail _validateEmail;
+
+        public SMSNotificationService(string token, string email)
+        {
+            _validateEmail = new ValidateEmail(email);
+            _token = token;
+        }
+
+        public bool Send(string msg, string email)
+        {
+            Console.WriteLine("teste de envio de SMS");
+            if (_token == "1234" && _validateEmail.Validate())
+            {
+                Console.WriteLine($"{msg} para: {email}");
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class ValidateEmail
+    {
+        public ValidateEmail(string email)
+        {
+            
+        }
+
+        public bool Validate()
+        {
+            return true;
+        }
+    }
+
+}
---
+++ NotificationManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Princípio da Inversão de Dependência (DIP)
+    // Injeção de Dependência (DI),
+    //Inversão de Controle (IOC)
+    public class NotificationManager
+    {
+        private INotificationService _notificationService;
+
+        public NotificationManager(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public bool Notify(string msg, string email)
+        {
+            return _notificationService.Send(msg, email);
+        }
+    }
+
+    public class Teste
+    {
+        private INotificationService _notificationService;
+
+        public Teste(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+    }
+
+    public class Teste1
+    {
+        private INotificationService _notificationService;
+
+        public Teste1(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+    }
+
+
+}
