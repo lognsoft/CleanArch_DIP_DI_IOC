17045ddfc026785a2cdc17ba59a8abd5ed0b8aae
BrunoBafilli <brunobafilli@gmail.com>
2024-04-19 19:55:01 -0300

    Solucao
    Solucao

---
+++ Código solucionado/EmailNotificationService .cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArch_DIP_DI_IOC;
+
+namespace ConsoleApp1
+{
+    public class EmailNotificationService : INotificationService
+    {
+        private IValidate _validateEmail;
+
+        public EmailNotificationService(IValidate validateEmail)
+        {
+            _validateEmail = validateEmail;
+        }
+
+        public bool Send(string msg, string email, string token)
+        {
+            if (token == "1234" && _validateEmail.Validate())
+            {
+                Console.WriteLine($"{msg} para: {email}");
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class ValidateEmail : IValidate
+    {
+        public ValidateEmail(string email)
+        {
+            
+        }
+
+        public bool Validate()
+        {
+            return true;
+        }
+    }
+
+}
---
+++ Código solucionado/IValidate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch_DIP_DI_IOC
+{
+    public interface IValidate
+    {
+        bool Validate();
+    }
+}
